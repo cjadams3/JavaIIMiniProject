65d7064c5b5ce0529347436ed73ea891ed02cb28
Bones707 <matthewsgerling@gmail.com>
2019-02-24 13:49:00 -0600

    Edit Phone started

---
+++ src/controller/PhoneHelper.java
@@ -0,0 +1,27 @@
+package controller;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.Phone;
+
+public class PhoneHelper {
+	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("");
+	
+	public Phone findPhoneByID(int idToEdit) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		Phone found = em.find(Phone.class, idToEdit);
+		em.close();
+		return found;
+	}
+	
+	public void updatePhone(Phone toEdit) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.merge(toEdit);
+		em.getTransaction().commit();
+		em.close();
+	}
+}
